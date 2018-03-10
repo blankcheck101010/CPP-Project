4b5d37990cc2c08cf5f9d3d9c5bd6588465bfee3
blankcheck101010 <saulalvarez86@gmail.com>
2018-03-10 12:27:07 -0800

    Adding sublime text and cpp files to head master

---
+++ C-Plus_Plus_Test/main.cpp
@@ -0,0 +1,10 @@
+// https://www.youtube.com/watch?v=hxTQdDJ51Kk
+#include <iostream>
+#include "my_math.h"
+
+int main()
+{
+ int x = factorial(5);
+ std::cout << "Hello, factorial(5) = " << x << std::endl;
+ return 0;
+}
